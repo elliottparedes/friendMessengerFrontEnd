b245a212355cb3a8faff583e8fea6c27d28799cf
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-03-01 21:29:34 -0600

    refactored register page

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -13,7 +13,11 @@ export default class login extends Component
 {
 
       
-     state = {loggedIn:false}
+     state = {loggedIn:false,
+              usernameFound:true,
+              passwordFound:true,
+              navigateToRegister:false          
+    }
 
       HandleSubmit =(event) =>{
         event.preventDefault();
@@ -22,16 +26,27 @@ export default class login extends Component
         console.log(event.target.elements.password.value);
         console.log("this worked");
         try{
-        //need to add password authorization, right now /login route only uses username. 
-        axios.post("https://friendmessenger.herokuapp.com/login", {email:event.target.elements.email.value}).then(
+
+        axios.post("https://friendmessenger.herokuapp.com/login", {email:event.target.elements.email.value,password:event.target.elements.password.value}).then(
           res=>{ 
             console.log(res);
             console.log(res.data)
+            if(res.data.message ==="Could not find username in Database")
+            {
+              this.setState({usernameFound:false})
+            }
+            else if(res.data.message==="password invalid")
+            {
+              this.setState({passwordFound:false})
+            }
+            else{
             localStorage.setItem("JWTKEY",res.data.token);
             sessionStorage.setItem("JWTKEY",res.data.token);
             this.setState({loggedIn: true});
             localStorage.setItem("user",res.data.username);
             sessionStorage.setItem("user",res.data.username);
+            }
+
           
           }
         );
@@ -42,6 +57,13 @@ export default class login extends Component
         }
         
       }
+
+      handleRegister = () =>
+      {
+        
+        this.setState({navigateToRegister:true}, ()=>console.log(this.state.navigateToRegister))
+        
+      }
     
     render()
     {
@@ -50,11 +72,17 @@ export default class login extends Component
         
        return <Navigate to="/" replace ={true}/>
       }
+      if(this.state.navigateToRegister)
+      {
+       
+        return <Navigate to="/register"/>
+        
+      }
       return (
       <div style={{height:"100vh", backgroundColor:"rgb(28,141,248)"}}>
     
          
-        <Container className="ml-0 mr-0 pl-0 pr-0" style ={{display:"flex", justifyContent:"center",alignItems:"center", paddingTop:"4rem"}}>
+        <Container className="ml-0 mr-0 pl-0 pr-0" style ={{display:"flex", justifyContent:"center",alignItems:"center", paddingTop:"2rem"}}>
           
           <Row className ="w-80">
             <Col className="border" style={{backgroundColor:"white", padding:"3rem", borderRadius:"30px"}}>
@@ -62,22 +90,36 @@ export default class login extends Component
             <h3 className=" mb-3 text-center">Sign In</h3>
             <Form.Group className="" controlId="formBasicEmail">
               <Form.Label className="" style={{fontWeight:"bold"}}>Email address</Form.Label>
+              <p style={{color:"red",display:this.state.usernameFound?"none":""}}>invalid username</p>
               <Form.Control className="mb-4" type="email" name="email" placeholder="Enter email" />
         
             </Form.Group>
       
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label  style={{fontWeight:"bold"}}>Password</Form.Label>
+              <p style={{color:"red",display:this.state.passwordFound?"none":""}}>incorrect password</p>
               <Form.Control className="mb-4" type="password" name="password" placeholder="Enter password" />
               </Form.Group>
             <Col className="text-center">
             <Button className="mb-4 w-100" variant="primary" type="submit">
                 Submit
             </Button>
+
+            
             </Col>
+            </form>
+            
+              <Col className="text-center">
+            <p style={{fontWeight:"bold"}}>Dont have an account?</p>
+            <Button onClick={this.handleRegister} style={{backgroundColor:"#A865C9"}} className="mb-4 w-100" variant="button" type="submit">
+                Register
+            </Button>
+            </Col>
+          
+            
             
           
-          </form>
+          
             </Col>
               
           </Row>
--- src/routes/register.jsx
+++ src/routes/register.jsx
@@ -1,21 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-// import {Link} from "react-router-dom";
+ import Container from 'react-bootstrap/Container';
+ import Row from 'react-bootstrap/Row';
+ import Col from 'react-bootstrap/Col';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-// import {useState} from 'react';
 import axios from 'axios';
 import React, {Component} from 'react';
 import {Navigate} from "react-router-dom";
 
 export default class register extends Component 
 {
-   state = {registered:false};
+   state = {
+     registered:false,
+     validUsername:true,
+     validPassword:true,
+     validEmail:true
+   };
 
-        handleSubmit = (event) => {
+        handleSubmit = (event) => 
+        {
+        
         event.preventDefault();
+        console.log("handlesubmit was called")
         console.log(event.target.elements.username.value);
         console.log(event.target.elements.password.value);
         console.log("this worked");
@@ -36,50 +42,61 @@ export default class register extends Component
         }
     
       }
-    
+      
     render(){
-      if(this.state.registered)
-      return <Navigate to="/login" replace ={true}/>
+       if(this.state.registered)
+       return <Navigate to="/login" />
 
       return (
-      <div>
+        <div style={{backgroundColor:"rgb(28,141,248)"}}>
     
-         <h2>This is the Register page</h2>
-         {/* <Link to="/invoices"> Invoices</Link> 
-    
-          <Link to="/expenses"> Expenses</Link>
-         */}
-        <form onSubmit={this.handleSubmit}>
-        
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" name="email" placeholder="Enter email" />
-          </Form.Group>
          
-          <Form.Group className="mb-3" controlId="formBasicUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="username" name="username" placeholder="Enter username" />
-      
-          </Form.Group>
-
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" name="password" placeholder="Password" />
+        <Container className="ml-0 mr-0 pl-0 pr-0 pb-4" style ={{height:"100vh",display:"flex", justifyContent:"center",alignItems:"center", paddingTop:"2rem"}}>
+          
+          <Row className ="w-80">
+            <Col className="border" style={{backgroundColor:"white", padding:"3rem", borderRadius:"30px"}}>
+              <form onSubmit={this.handleSubmit}>
+            <h3 className=" mb-3 text-center">Sign Up</h3>
+           
+            <Form.Group className="" controlId="formBasicEmail">
+              <Form.Label className="" style={{fontWeight:"bold"}}>Email address</Form.Label>
+              <p style={{color:"red",display:this.state.validEmail?"none":""}}>invalid username</p>
+              <Form.Control className="mb-4" type="email" name="email" placeholder="Enter email" />
             </Form.Group>
-    
-          <Button variant="primary" type="submit">
-              Submit
-          </Button>
+
+            <Form.Group className="" controlId="">
+              <Form.Label className="" style={{fontWeight:"bold"}}>Username</Form.Label>
+              <p style={{color:"red",display:this.state.validUsername?"none":""}}>invalid username</p>
+              <Form.Control className="mb-4" type="" name="username" placeholder="Enter username" />
+            </Form.Group>
+      
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label  style={{fontWeight:"bold"}}>Password</Form.Label>
+              <p style={{color:"red",display:this.state.validPassword?"none":""}}>incorrect password</p>
+              <Form.Control className="mb-4" type="password" name="password" placeholder="Enter password" />
+            </Form.Group>
+
+            <Col className="text-center">
+            <Button className="mb-4 w-100" variant="primary" type="submit">
+                Submit
+            </Button>
+
+            
+            </Col>
+            </form> 
+            </Col>
+              
+          </Row>
+          
+       
+        </Container>
         
-        </form>
     
     
     
       </div>
-    
-     
-    
+
+
       ); }
 }
 
